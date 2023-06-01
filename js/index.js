31d8f058ffefdb8c62085f86077fae8e92f91a93
Marcos Romero <marcromerogar4@gmail.com>
2023-06-01 15:17:34 -0500

    Recuperacion de contraseña añadida a las vistas

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 let cerrarMenu = document.querySelector('#cerrar--menu');
 let menuNav = document.querySelector('#menu--nav')
 let abrirMenu = document.querySelector('#abrir--menu')
+let confirmacionCorreo = document.querySelector('.confirmacion__correo')
+let enviarCorreo = document.querySelector('#enviar__correo')
+let correoLinea = document.querySelector('#correo__linea')
+let loginRecuperar = document.querySelector('.login__recuperar')
+
+
+enviarCorreo.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (correoLinea.value === '') {
+        alert('Por favor, ingrese su correo electrónico.');
+    } else if (!correoLinea.validity.valid) {
+        alert('Por favor, ingrese un correo electrónico válido.');
+    } else {
+        confirmacionCorreo.classList.toggle('hidden__correo');
+        loginRecuperar.classList.toggle('hidden__correo')
+    }
+
+
+})
+
+
 
 // Agregar evento para cerrar el menú al hacer clic en el botón X
 cerrarMenu.addEventListener('click', () => {
@@ -26,3 +48,5 @@ verificarTamanoPantalla();
 window.addEventListener('resize', () => {
     verificarTamanoPantalla();
 })
+
+
